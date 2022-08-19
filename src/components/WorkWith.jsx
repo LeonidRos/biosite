a437114c980473f652d493bc9e095a4114b43a3f
LeonidRos <102150359+LeonidRos@users.noreply.github.com>
2022-08-19 04:33:56 +0300

    added swipe, made adaptive to 768px

--- src/components/WorkWith.jsx
+++ src/components/WorkWith.jsx
@@ -1,16 +1,37 @@
 import homeImg1 from '../img/wood_oak.png';
 import homeImg2 from '../img/wood_buk.png';
 import homeImg3 from '../img/wood_ash.png';
+import { useRef, useState } from 'react';
 
 function WorkWith() {
+
+    const swipe = useRef(null);
+    const [swipePosition, setSwipePosition] = useState(0);
+
+
+    function touchMove(e){
+
+        if(swipe.current.clientWidth > 1080) return;
+        setSwipePosition(e.touches[0].clientX - 530);
+    }
+
+    const transform = {
+        transform: `translateX(${swipePosition}px)`,
+        transition: '0.4s'
+    }
+
+    const transformScroll = {
+        transform: `translateX(${-swipePosition}px)`,
+    }
+
 	return (
     <section className="with">
 
        <h2 className="prime_h2">The wood we work with</h2>
 
-       <div className="with__woods">
+       <div className="with__woods" ref={swipe} onTouchMove={touchMove}>
 
-            <div className="with__woods_item">
+            <div style={transform} className="with__woods_item">
                 <img src={homeImg1} alt="#" />
                 <h3>Oak</h3>
 
@@ -23,7 +44,7 @@ function WorkWith() {
 
             </div>
 
-            <div className="with__woods_item">
+            <div style={transform} className="with__woods_item">
                 <img src={homeImg2} alt="#" />
                 <h3>Buk</h3>
                 
@@ -34,7 +55,7 @@ function WorkWith() {
 
             </div>
 
-            <div className="with__woods_item">
+            <div style={transform} className="with__woods_item">
                 <img src={homeImg3} alt="#" />
                 <h3>Ash</h3>
                 
@@ -43,6 +64,9 @@ function WorkWith() {
                     <li>Hard to handle</li>
                 </ul>
 
+            </div>
+            <div className="scroll__bar">
+                <div style={transformScroll} className="scroll__swiper"></div>
             </div>
        </div>
     </section>
